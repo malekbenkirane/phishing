455a0925c758245284a1b7ab1f059e455c393948
Admin malek <adminmalek@regence.ca>
2025-04-01 13:19:24 -0400

    deriner commit

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ def download_pdf():
     pdf.output("report.pdf")
     return send_file("report.pdf", as_attachment=True)
     
- def clean_text(text):
+def clean_text(text):
     return text.replace('’', "'").replace('“', '"').replace('”', '"')
 
 # Utiliser clean_text pour nettoyer les données avant de les ajouter au PDF
